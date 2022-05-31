6a2bdf3f694e4d747f566fb39a55cbb1b5dc7dfe
Yuya Iwabuchi <yuya.iwabuchi@gmail.com>
2022-05-31 02:40:34 -0400

    Refactored unsupported browser message

--- src/steps/InitialStep.tsx
+++ src/steps/InitialStep.tsx
@@ -26,36 +26,34 @@ const InitialStep = ({ handleNextStep }: { handleNextStep: Function }) => {
     [],
   )
 
-  if (!BrowserSpeechRecognition || isIosChrome) {
-    return (
-      <main className="mx-auto h-full flex justify-center items-center">
-        <div className="text-center font-medium text-xl text-red-500 dark:text-red-400 mb-10">
-          This browser does not support speech recognition.
-          <br />
-          Try using Chrome for Desktop, Chrome for Android, or Safari for iOS instead.
-        </div>
-      </main>
-    )
-  }
-
   return (
     <>
       <div className="text-lg">Say the correct phrase of the abbreviation in the given time to win the rounds!</div>
       <div className="grow flex flex-col justify-center items-center">
-        <button
-          onClick={handleStartGame}
-          className="bg-blue-300 dark:bg-blue-600 text-blue-800 dark:text-blue-100 text-6xl font-semibold px-10 py-5 rounded-full transition ease-in-out hover:brightness-90 hover:scale-110"
-        >
-          START
-        </button>
-        {isMacosSafari && (
-          <div className="text-lg break-normal pt-3">
-            <span>On Safari Desktop, you need to enable Dictation from </span>
-            <span className="font-medium whitespace-nowrap">{'System Preference > Keyboard > Dictation'}</span>
-            <span> to use the speech recognition.</span>
+        {BrowserSpeechRecognition && !isIosChrome ? (
+          <>
+            <button
+              onClick={handleStartGame}
+              className="bg-blue-300 dark:bg-blue-600 text-blue-800 dark:text-blue-100 text-6xl font-semibold px-10 py-5 rounded-full transition ease-in-out hover:brightness-90 hover:scale-110"
+            >
+              START
+            </button>
+            {isMacosSafari && (
+              <div className="text-lg break-normal pt-3">
+                <span>On Safari Desktop, you need to enable Dictation from </span>
+                <span className="font-medium whitespace-nowrap">{'System Preference > Keyboard > Dictation'}</span>
+                <span> to use the speech recognition.</span>
+              </div>
+            )}
+            <div className="font-red-700 font-semibold pt-3">{isPermissionError && 'Permission was not granted.'}</div>
+          </>
+        ) : (
+          <div className="text-center font-medium text-xl text-red-500 dark:text-red-400 mb-10">
+            This browser does not support speech recognition.
+            <br />
+            Try using Chrome for Desktop, Chrome for Android, or Safari for iOS instead.
           </div>
         )}
-        <div className="font-red-700 font-semibold pt-3">{isPermissionError && 'Permission was not granted.'}</div>
       </div>
       <div className="mb-5">
         <div>
